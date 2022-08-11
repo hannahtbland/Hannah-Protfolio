91ca306cdc76940a3d837175c79d3fdb5f231e4d
hannahtbland <100818778+hannahtbland@users.noreply.github.com>
2022-08-10 18:45:00 -0600

    add js & analytics

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-console.log("your index file is loaded correctly");
-$(".option").click(function(){
-    $(".option").removeClass("active");
-    $(this).addClass("active");
-    
- });
+// When the user scrolls down 20px from the top of the document, show the button used from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("myBtn").style.display = "block";
+    } else {
+      document.getElementById("myBtn").style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
